793d5106aafba5d67a48e78d592045fad17961f2
Yuga <Trev@brennberry.com>
2023-05-04 18:54:59 -0400

    updated javascript

--- script.js
+++ script.js
@@ -8,6 +8,20 @@ $(function () {
     const timeBlockHour = parseInt($(this).attr('id').split('-')[1]);
     // gets the current hour
     const currentHour = dayjs().hour();
-
+    //  adds each class depending on the time block and current time
+    if (timeBlockHour < currentHour) {
+      $(this).addClass('past');
+    } else if (timeBlockHour === currentHour) {
+      $(this).addClass('present');
+    } else {
+      $(this).addClass('future');
+    }
+// retrieves the saved text from the selected hour
+const savedEvent = localStorage.getItem(`hour-${timeBlockHour}`);
+  if (savedEvent) {
+     $(this).find('textarea').val(savedEvent);
+    }
   });
+
 });
+
